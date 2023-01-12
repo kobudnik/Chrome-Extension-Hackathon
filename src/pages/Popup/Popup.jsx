a096a9484a4fbe83c2f6b50ec6a49fc1c84e55df
kobudnik <105399943+kobudnik@users.noreply.github.com>
2023-01-12 02:05:23 -0500

    Webpack reconfigured by yours truly

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
 const Popup = () => {
@@ -9,17 +8,6 @@ const Popup = () => {
       <header className="App-header">
         <h1> HELLO WORLD</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
       </header>
     </div>
   );
